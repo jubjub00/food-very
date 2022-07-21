0b637a7a80350a1938c394df477cab8e5bb7332f
chukeit panpiboon <chukeit@livingmobile.me>
2022-07-21 17:55:55 +0700

    init pwa

---
+++ firebase/public/precache-manifest.1496c3a6bc058faefdfa048649a33230.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfd8baec038e13b61cd3",
+    "url": "/css/app.1d5544e9.css"
+  },
+  {
+    "revision": "adff1c9f20ced597575ad78551d38425",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4c8d8153baddd53d3761",
+    "url": "/js/about.18b04715.js"
+  },
+  {
+    "revision": "bfd8baec038e13b61cd3",
+    "url": "/js/app.739b3384.js"
+  },
+  {
+    "revision": "8aa63e7363f44474fb69",
+    "url": "/js/chunk-vendors.70086f06.js"
+  },
+  {
+    "revision": "83a53ac8111b21c3fbb4c458bb4f1c81",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,25 @@
 <template>
   <div id="nav">
     <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link> |
     <router-link to="/about">About</router-link>
   </div>
   <router-view />
 </template>
 
 <style>
+body {
+  display: flex;
+  justify-content: center;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  width: -webkit-fill-available;
+  max-width: 500px;
 }
 
 #nav {
